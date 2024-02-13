bf84f11c440b406f7c5b450fbe78bb6caa420c13
Oladee <deemajor230600@gmail.com>
2024-02-13 01:22:13 +0100

    finished with the gaming logic

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
+let btn = document.querySelector('#buttons');
+let pscore = document.querySelector('#pscore')
+let cscore = document.querySelector('#cscore')
+let winner = document.querySelector('#winner')
+
 const getComputerChoice = ()=>{
     const arrayChoice = ["Rock", "Scissors", "Paper"]
     let choice = arrayChoice[Math.floor(Math.random()*3)]
     return choice
 
+}
+
+let playerscore = 0;
+let computerscore = 0;
+
+function checkAndAddScore(){
+    if (playerscore == 5 || computerscore == 5){
+        if (playerscore == 5){
+            winner.textContent = `Player wins!!`
+            playerscore = 0;
+            computerscore = 0;
+        }else {
+            winner.textContent = `computer wins!!`
+            playerscore = 0;
+            computerscore = 0;
+        }
+    }
 }
 
 let computerChoice = getComputerChoice()
@@ -13,26 +35,43 @@ const gameRound = (playerChoice, computersChoice)=> {
     console.log("com choice = " + computersChoice)
     
     if (playerChoice == computersChoice){
-        return "Wow, this was a tie"
+        winner.textContent = "Wow, this was a tie no score was added, please continue"
     }else if (playerChoice == "paper" && computersChoice == "rock"){
-        return "You Win!"
+        playerscore += 1
     }else if (playerChoice == "scissors" && computersChoice == "paper"){
-        return "You Win"
+        playerscore += 1
     }else if (playerChoice == "rock" && computersChoice == "scissors"){
-        return "You Win"
+        playerscore += 1
     }else{
-        return "Computer Wins !"
+        computerscore += 1
     }
+    pscore.textContent = `Player Score: ${playerscore}`
+    cscore.textContent = `Computer Score: ${computerscore}`
 }
 
-function playGame(){
-    let playersChoice = prompt("Kindly insert your object, Rock Paper or Scissors")
+function playGame(playersChoice){
+    if (winner.textContent){
+        winner.textContent = ""
+    }
     let computerChoice = getComputerChoice()
-    console.log(gameRound(playersChoice, computerChoice))
+    gameRound(playersChoice, computerChoice)
+    checkAndAddScore()
 }
 
-playGame()
-playGame()
-playGame()
-playGame()
+btn.addEventListener('click', (e)=>{
+    let target = e.target
+
+    switch (target) {
+        case rock:
+            playGame('rock')
+            break;
+        case scissors:
+            playGame('scissors')
+            break;
+        case paper:
+            playGame('paper')
+            break;
+    }
+    // console.log(target.id)
+})
 
